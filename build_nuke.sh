882d9da003f8a7ef1e78433df57412e7951679a2
Nicolas Modrzyk <hellonico@gmail.com>
2026-05-18 16:53:22 +0900

    feat: include latest git commit message in build info and update build timestamp format

--- build_nuke.sh
+++ build_nuke.sh
@@ -3,7 +3,12 @@ set -e
 mkdir -p .build
 cp main.coni .build/main.coni
 COMMIT=$(git rev-parse --short HEAD || echo "unknown")
-DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-sed -i '' "s/(def nuke-commit.*/(def nuke-commit \"$COMMIT\")/g" .build/main.coni
-sed -i '' "s/(def nuke-build-time.*/(def nuke-build-time \"$DATE\")/g" .build/main.coni
+DATE=$(date +"%Y-%m-%d %H:%M:%S %Z")
+MSG=$(git log -1 --format=%s || echo "")
+MSG=${MSG//\"/}
+
+sed -i '' "s/(def nuke-commit .*/(def nuke-commit \"$COMMIT\")/g" .build/main.coni
+sed -i '' "s/(def nuke-build-time .*/(def nuke-build-time \"$DATE\")/g" .build/main.coni
+sed -i '' "s/(def nuke-commit-msg .*/(def nuke-commit-msg \"$MSG\")/g" .build/main.coni
+
 CONI_HOME=/Users/nico/cool/coni-lang PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 /tmp/coni-compiler build .build/main.coni -o nuke
