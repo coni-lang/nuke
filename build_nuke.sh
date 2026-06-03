0e5dcc71f08323ac61e38bbf89b2d6bb1c152e81
Nicolas Modrzyk <hellonico@gmail.com>
2026-06-03 10:34:57 +0900

    ci: run on ubuntu-latest and use setup-coni action for compiler

--- build_nuke.sh
+++ build_nuke.sh
@@ -7,17 +7,20 @@ DATE=$(date +"%Y-%m-%d %H:%M:%S %Z")
 MSG=$(git log -1 --format=%s || echo "")
 MSG=${MSG//\"/}
 
-sed -i '' "s~(def nuke-commit .*~(def nuke-commit \"$COMMIT\")~g" .build/main.coni
-sed -i '' "s~(def nuke-build-time .*~(def nuke-build-time \"$DATE\")~g" .build/main.coni
-sed -i '' "s~(def nuke-commit-msg .*~(def nuke-commit-msg \"$MSG\")~g" .build/main.coni
+sed -i.bak "s~(def nuke-commit .*~(def nuke-commit \"$COMMIT\")~g" .build/main.coni
+sed -i.bak "s~(def nuke-build-time .*~(def nuke-build-time \"$DATE\")~g" .build/main.coni
+sed -i.bak "s~(def nuke-commit-msg .*~(def nuke-commit-msg \"$MSG\")~g" .build/main.coni
+rm -f .build/main.coni.bak
+
+COMPILER=${CONI_COMPILER:-./coni-compiler}
 
 if [ "$BUILD_ALL" = "1" ]; then
-    CONI_HOME=${CONI_HOME:-/Users/nico/cool/coni-lang} PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 ./coni-compiler build .build/main.coni -o nuke-mac
-    CONI_HOME=${CONI_HOME:-/Users/nico/cool/coni-lang} PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 GOOS=linux GOARCH=amd64 ./coni-compiler build .build/main.coni -o nuke-linux
-    CONI_HOME=${CONI_HOME:-/Users/nico/cool/coni-lang} PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 GOOS=windows GOARCH=amd64 ./coni-compiler build .build/main.coni -o nuke.exe
+    CONI_HOME=${CONI_HOME:-/Users/nico/cool/coni-lang} PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 $COMPILER build .build/main.coni -o nuke-mac
+    CONI_HOME=${CONI_HOME:-/Users/nico/cool/coni-lang} PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 GOOS=linux GOARCH=amd64 $COMPILER build .build/main.coni -o nuke-linux
+    CONI_HOME=${CONI_HOME:-/Users/nico/cool/coni-lang} PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 GOOS=windows GOARCH=amd64 $COMPILER build .build/main.coni -o nuke.exe
     cp nuke-mac nuke
 else
-    CONI_HOME=${CONI_HOME:-/Users/nico/cool/coni-lang} PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 ./coni-compiler build .build/main.coni -o nuke
+    CONI_HOME=${CONI_HOME:-/Users/nico/cool/coni-lang} PATH="$PATH:/usr/local/go/bin:/opt/homebrew/bin" CGO_ENABLED=0 $COMPILER build .build/main.coni -o nuke
 fi
 
 echo "Running smoke test to verify syntax and parsing..."
